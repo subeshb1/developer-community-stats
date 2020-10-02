f9e0ba714b348efb428b90066d1d18a3e174755a
Subesh Bhandari <subeshb1@gmail.com>
2020-10-02 17:14:55 +0545

    Add contributors js

---
+++ contributors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "subeshb1": {
+    name: "Subesh Bhandari",
+    twitter: "https://twitter.com/subeshb1",
+    linkedin: "https://www.linkedin.com/in/subesh-bhandari-523438112/",
+  },
+}
