7700a7cc4b5039e104a235bc1807c6d6ddc9a519
Subesh Bhandari <subeshb1@gmail.com>
2020-10-13 18:02:31 +0545

    Fetch data in batch

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const contributors = require('./contributors.json')
+const contributorsList = require('./contributors.json')
 let stats
 if (process.env.ENV_TYPE === 'mock') {
   stats = require('./mock-stats-generator')
@@ -16,24 +16,35 @@ exports.sourceNodes = async ({
 }) => {
   const { createNode } = actions
 
-  return stats.fetchCountStats(Object.keys(contributors)).then(contributorStats => {
-    // loop through data and create Gatsby nodes
-    Object.entries(contributorStats).forEach(([contributor, information]) => {
+  return Promise.all(
+    Object.keys(contributorsList).reduce((batch, item) => {
+      if (batch[batch.length - 1].length === 20) {
+        batch.push([item])
+      } else {
+        batch[batch.length - 1].push(item)
+      }
+      return batch
+    }, [[]])
+      .map(contributors => stats.fetchCountStats(contributors))
+  ).then((allContributors) => allContributors.reduce((obj, nobj) => ({ ...obj, ...nobj }), {}))
+    .then(contributorStats => {
+      // loop through data and create Gatsby nodes
+      Object.entries(contributorStats).forEach(([contributor, information]) => {
 
-      createNode({
-        ...information,
-        ...contributors[contributor],
-        githubUserId: contributor,
-        id: createNodeId(`${contributor}`),
-        countryCode: findCountryCode(contributors[contributor].country),
-        parent: null,
-        children: [],
-        internal: {
-          type: 'contributor',
-          content: JSON.stringify(information),
-          contentDigest: createContentDigest(contributor),
-        },
+        createNode({
+          ...information,
+          ...contributorsList[contributor],
+          githubUserId: contributor,
+          id: createNodeId(`${contributor}`),
+          countryCode: findCountryCode(contributorsList[contributor].country),
+          parent: null,
+          children: [],
+          internal: {
+            type: 'contributor',
+            content: JSON.stringify(information),
+            contentDigest: createContentDigest(contributor),
+          },
+        })
       })
-    })
-  }).catch(console.error)
+    }).catch(console.error)
 }
