3606734a356b61456310dc0f589e1adba060c29e
Subesh Bhandari <subeshb1@gmail.com>
2020-10-14 10:59:14 +0545

    Add link to card to profile

--- src/components/DeveloperCard/index.jsx
+++ src/components/DeveloperCard/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { GrLinkedin, GrTwitter, GrGithub } from 'react-icons/gr'
 import { FaLink } from 'react-icons/fa'
+import { Link } from 'gatsby'
 
 export default function DeveloperCard({
   repositoryCount,
@@ -21,71 +22,77 @@ export default function DeveloperCard({
   website,
 }) {
   return (
-    <div className="developer-card">
-      <div className="developer-card-user-info">
-        {countryCode && (
-          <span
-            className={`flag-icon flag-icon-${countryCode.toLowerCase()} `}
-            title={country}
-          ></span>
-        )}
-        <div className="developer-card-username">{githubUserId}</div>
-        <div className="developer-card-avatar">
-          <img src={avatarUrl} alt={githubUserId} />
-        </div>
-        <div className="developer-card-links">
-          <a
-            href={`https://github.com/${githubUserId}`}
-            target="__blank"
-            rel="noreferrer"
-          >
-            <GrGithub />
-          </a>
-          {twitter && (
-            <a href={twitter} target="__blank" rel="noreferrer">
-              <GrTwitter />
-            </a>
+    <Link
+      className="developer-card-link"
+      id="colorless-link"
+      to={`/profile/${githubUserId}`}
+    >
+      <div className="developer-card">
+        <div className="developer-card-user-info">
+          {countryCode && (
+            <span
+              className={`flag-icon flag-icon-${countryCode.toLowerCase()} `}
+              title={country}
+            ></span>
           )}
-          {linkedin && (
-            <a href={linkedin} target="__blank" rel="noreferrer">
-              <GrLinkedin />
+          <div className="developer-card-username">{githubUserId}</div>
+          <div className="developer-card-avatar">
+            <img src={avatarUrl} alt={githubUserId} />
+          </div>
+          <div className="developer-card-links">
+            <a
+              href={`https://github.com/${githubUserId}`}
+              target="__blank"
+              rel="noreferrer"
+            >
+              <GrGithub />
             </a>
-          )}
-          {website && (
-            <a href={website} target="__blank" rel="noreferrer">
-              <FaLink />
-            </a>
-          )}
+            {twitter && (
+              <a href={twitter} target="__blank" rel="noreferrer">
+                <GrTwitter />
+              </a>
+            )}
+            {linkedin && (
+              <a href={linkedin} target="__blank" rel="noreferrer">
+                <GrLinkedin />
+              </a>
+            )}
+            {website && (
+              <a href={website} target="__blank" rel="noreferrer">
+                <FaLink />
+              </a>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="developer-card-user-stats">
-        <div className="developer-card-name">{name}</div>
-        <div className="developer-card-detail">
-          <div className="developer-card-title">Contribution(This Year)</div>
-          <div className="developer-card-value">{thisYearContribution}</div>
-        </div>
-        <div className="developer-card-detail">
-          <div className="developer-card-title">No. of Repositories</div>
-          <div className="developer-card-value">{repositoryCount}</div>
-        </div>
-        <div className="developer-card-detail">
-          <div className="developer-card-title">No. of Repos Contributed</div>
-          <div className="developer-card-value">{repoContributedCount}</div>
-        </div>
+        <div className="developer-card-user-stats">
+          <div className="developer-card-name">{name}</div>
+          <div className="developer-card-detail">
+            <div className="developer-card-title">Contribution(This Year)</div>
+            <div className="developer-card-value">{thisYearContribution}</div>
+          </div>
+          <div className="developer-card-detail">
+            <div className="developer-card-title">No. of Repositories</div>
+            <div className="developer-card-value">{repositoryCount}</div>
+          </div>
+          <div className="developer-card-detail">
+            <div className="developer-card-title">No. of Repos Contributed</div>
+            <div className="developer-card-value">{repoContributedCount}</div>
+          </div>
 
-        <div className="developer-card-detail">
-          <div className="developer-card-title">Total Pull Requests</div>
-          <div className="developer-card-value">{pullRequestsCount}</div>
-        </div>
-        <div className="developer-card-detail">
-          <div className="developer-card-title">Total Issues</div>
-          <div className="developer-card-value">{issuesCount}</div>
-        </div>
-        <div className="developer-card-detail">
-          <div className="developer-card-title">Followers</div>
-          <div className="developer-card-value">{followersCount}</div>
+          <div className="developer-card-detail">
+            <div className="developer-card-title">Total Pull Requests</div>
+            <div className="developer-card-value">{pullRequestsCount}</div>
+          </div>
+          <div className="developer-card-detail">
+            <div className="developer-card-title">Total Issues</div>
+            <div className="developer-card-value">{issuesCount}</div>
+          </div>
+          <div className="developer-card-detail">
+            <div className="developer-card-title">Followers</div>
+            <div className="developer-card-value">{followersCount}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
