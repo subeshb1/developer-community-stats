dad63df216ec3f08c7067e57487a63ac402ecb4b
Subesh Bhandari <subeshb1@gmail.com>
2020-10-14 10:25:12 +0545

    Create page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 const contributorsList = require('./contributors.json')
 let stats
 if (process.env.ENV_TYPE === 'mock') {
@@ -7,6 +9,74 @@ if (process.env.ENV_TYPE === 'mock') {
 }
 
 const findCountryCode = require('./country-codes')
+
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const blogPost = path.resolve(`./src/templates/readme-profile.js`)
+  return graphql(
+    `
+      {
+        allMarkdownRemark(
+          limit: 1000
+        ) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog posts pages.
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach((post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
+
+      createPage({
+        path: post.node.fields.slug,
+        component: blogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
+      })
+    })
+  })
+}
+
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    let value
+    if (node.frontmatter.type === 'doc') {
+      value = `${createFilePath({ node, getNode }).replace(/\/$/, '')}`
+    } else {
+      value = `/profile${createFilePath({ node, getNode }).replace(/\/$/, '')}`
+    }
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+
 
 exports.sourceNodes = async ({
   actions,
