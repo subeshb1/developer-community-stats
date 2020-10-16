32cf5a3c6273d360918a1c62a780d7a2b2c8644e
Subesh Bhandari <subeshb1@gmail.com>
2020-10-16 21:39:35 +0545

    Add number

--- src/components/DeveloperCard/index.jsx
+++ src/components/DeveloperCard/index.jsx
@@ -23,80 +23,79 @@ export default function DeveloperCard({
   position,
 }) {
   return (
-    <Link
-      className="developer-card-link"
-      id="colorless-link"
-      to={`/profile/${githubUserId}`}
-    >
-      <div className="developer-card">
-        <div className="developer-card-user-info">
-          {countryCode && (
-            <span
-              className={`flag-icon flag-icon-${countryCode.toLowerCase()} `}
-              title={country}
-            ></span>
-          )}
-          <div className="developer-card-username">{githubUserId}</div>
-          <div className="developer-card-avatar">
-            <img src={avatarUrl} alt={githubUserId} />
-          </div>
-          <div className="developer-card-links">
-            <a
-              href={`https://github.com/${githubUserId}`}
-              target="__blank"
-              rel="noreferrer"
-            >
-              <GrGithub />
-            </a>
-            {twitter && (
-              <a href={twitter} target="__blank" rel="noreferrer">
-                <GrTwitter />
-              </a>
-            )}
-            {linkedin && (
-              <a href={linkedin} target="__blank" rel="noreferrer">
-                <GrLinkedin />
-              </a>
-            )}
-            {website && (
-              <a href={website} target="__blank" rel="noreferrer">
-                <FaLink />
-              </a>
-            )}
-          </div>
+    <div className="developer-card">
+      <div className="developer-card-user-info">
+        {countryCode && (
+          <span
+            className={`flag-icon flag-icon-${countryCode.toLowerCase()} `}
+            title={country}
+          ></span>
+        )}
+        <div className="developer-card-username">{githubUserId}</div>
+        <div className="developer-card-avatar">
+          <img src={avatarUrl} alt={githubUserId} />
         </div>
-        <div className="developer-card-user-stats">
-          <div className="developer-card-name">
-            {name}
-            {position && <span># {position}</span>}
-          </div>
-          <div className="developer-card-detail">
-            <div className="developer-card-title">Contribution(This Year)</div>
-            <div className="developer-card-value">{thisYearContribution}</div>
-          </div>
-          <div className="developer-card-detail">
-            <div className="developer-card-title">No. of Repositories</div>
-            <div className="developer-card-value">{repositoryCount}</div>
-          </div>
-          <div className="developer-card-detail">
-            <div className="developer-card-title">No. of Repos Contributed</div>
-            <div className="developer-card-value">{repoContributedCount}</div>
-          </div>
-
-          <div className="developer-card-detail">
-            <div className="developer-card-title">Total Pull Requests</div>
-            <div className="developer-card-value">{pullRequestsCount}</div>
-          </div>
-          <div className="developer-card-detail">
-            <div className="developer-card-title">Total Issues</div>
-            <div className="developer-card-value">{issuesCount}</div>
-          </div>
-          <div className="developer-card-detail">
-            <div className="developer-card-title">Followers</div>
-            <div className="developer-card-value">{followersCount}</div>
-          </div>
+        <div className="developer-card-links">
+          <a
+            href={`https://github.com/${githubUserId}`}
+            target="__blank"
+            rel="noreferrer"
+          >
+            <GrGithub />
+          </a>
+          {twitter && (
+            <a href={twitter} target="__blank" rel="noreferrer">
+              <GrTwitter />
+            </a>
+          )}
+          {linkedin && (
+            <a href={linkedin} target="__blank" rel="noreferrer">
+              <GrLinkedin />
+            </a>
+          )}
+          {website && (
+            <a href={website} target="__blank" rel="noreferrer">
+              <FaLink />
+            </a>
+          )}
         </div>
       </div>
-    </Link>
+      <div className="developer-card-user-stats">
+        <Link
+          className="developer-card-link"
+          id="colorless-link"
+          to={`/profile/${githubUserId}`}
+        ></Link>
+        <div className="developer-card-name">
+          {name}
+          {position && <span># {position}</span>}
+        </div>
+        <div className="developer-card-detail">
+          <div className="developer-card-title">Contribution(This Year)</div>
+          <div className="developer-card-value">{thisYearContribution}</div>
+        </div>
+        <div className="developer-card-detail">
+          <div className="developer-card-title">No. of Repositories</div>
+          <div className="developer-card-value">{repositoryCount}</div>
+        </div>
+        <div className="developer-card-detail">
+          <div className="developer-card-title">No. of Repos Contributed</div>
+          <div className="developer-card-value">{repoContributedCount}</div>
+        </div>
+
+        <div className="developer-card-detail">
+          <div className="developer-card-title">Total Pull Requests</div>
+          <div className="developer-card-value">{pullRequestsCount}</div>
+        </div>
+        <div className="developer-card-detail">
+          <div className="developer-card-title">Total Issues</div>
+          <div className="developer-card-value">{issuesCount}</div>
+        </div>
+        <div className="developer-card-detail">
+          <div className="developer-card-title">Followers</div>
+          <div className="developer-card-value">{followersCount}</div>
+        </div>
+      </div>
+    </div>
   )
 }
