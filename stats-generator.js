cc81d600b9ba77dab7fabace83659cd915062f6a
Subesh Bhandari <subeshb1@gmail.com>
2020-10-21 07:06:11 +0545

    Remove null

--- stats-generator.js
+++ stats-generator.js
@@ -51,7 +51,7 @@ const extractGraphqlJson = res => {
     if (res.errors && res.errors.length > res.data.length) {
       throw res
     }
-    return res
+    return res.filter(x => x !== null)
   })
 }
 
