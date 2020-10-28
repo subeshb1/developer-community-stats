43491bfe8644d2d852a33631d610301e7d9633ab
vijayakumar.s <vijayakumar.s.contractor@pepsico.com>
2020-10-28 14:13:30 +0530

    mock stats updated

--- mock-stats-generator.js
+++ mock-stats-generator.js
@@ -8,10 +8,9 @@ module.exports = {
         followersCount: 1,
         issuesCount: 0,
         pullRequestsCount: 6,
-        avatarUrl:
-          'https://avatars1.githubusercontent.com/u/70329145?u=35847772b4aaca38d0b9ffe40f593020c79287a7&v=4',
+        avatarUrl: 'https://avatars1.githubusercontent.com/u/70329145?u=35847772b4aaca38d0b9ffe40f593020c79287a7&v=4',
         firstContribution: 2020,
-        thisYearContribution: 32,
+        thisYearContribution: 32
       },
       acorson1: {
         repositoryCount: 4,
@@ -22,7 +21,7 @@ module.exports = {
         pullRequestsCount: 4,
         avatarUrl: 'https://avatars2.githubusercontent.com/u/33963426?v=4',
         firstContribution: 2017,
-        thisYearContribution: 46,
+        thisYearContribution: 46
       },
       J3ffJessie: {
         repositoryCount: 43,
@@ -31,10 +30,9 @@ module.exports = {
         followersCount: 21,
         issuesCount: 7,
         pullRequestsCount: 19,
-        avatarUrl:
-          'https://avatars1.githubusercontent.com/u/39473092?u=6a37a9dfaf21f2412f6881fcf3f1009e26078e8d&v=4',
+        avatarUrl: 'https://avatars1.githubusercontent.com/u/39473092?u=6a37a9dfaf21f2412f6881fcf3f1009e26078e8d&v=4',
         firstContribution: 2018,
-        thisYearContribution: 260,
+        thisYearContribution: 260
       },
       nisargpawade: {
         repositoryCount: 0,
@@ -45,7 +43,7 @@ module.exports = {
         pullRequestsCount: 5,
         avatarUrl: 'https://avatars3.githubusercontent.com/u/70917826?v=4',
         firstContribution: 2020,
-        thisYearContribution: 12,
+        thisYearContribution: 12
       },
       OmkarGeedh: {
         repositoryCount: 6,
@@ -56,32 +54,23 @@ module.exports = {
         pullRequestsCount: 7,
         avatarUrl: 'https://avatars0.githubusercontent.com/u/40747153?v=4',
         firstContribution: 2018,
-        thisYearContribution: 39,
+        thisYearContribution: 39
       },
       PankajGupta: {
         repositoryCount: 7,
         repoContributedCount: 0,
         contributionYears: [
-          2020,
-          2019,
-          2018,
-          2017,
-          2016,
-          2015,
-          2014,
-          2013,
-          2012,
-          2011,
-          2010,
-          2009,
+          2020, 2019, 2018,
+          2017, 2016, 2015,
+          2014, 2013, 2012,
+          2011, 2010, 2009
         ],
         followersCount: 47,
         issuesCount: 40,
         pullRequestsCount: 22,
-        avatarUrl:
-          'https://avatars3.githubusercontent.com/u/71128?u=a3a9ade8081a829959859c1b77b7a0c3d5e0e04e&v=4',
+        avatarUrl: 'https://avatars3.githubusercontent.com/u/71128?u=a3a9ade8081a829959859c1b77b7a0c3d5e0e04e&v=4',
         firstContribution: 2009,
-        thisYearContribution: 0,
+        thisYearContribution: 0
       },
       prajnakalpa: {
         repositoryCount: 1,
@@ -92,7 +81,7 @@ module.exports = {
         pullRequestsCount: 17,
         avatarUrl: 'https://avatars0.githubusercontent.com/u/56736266?v=4',
         firstContribution: 2019,
-        thisYearContribution: 51,
+        thisYearContribution: 51
       },
       'Shubham-Agarwal16': {
         repositoryCount: 6,
@@ -101,10 +90,9 @@ module.exports = {
         followersCount: 0,
         issuesCount: 2,
         pullRequestsCount: 5,
-        avatarUrl:
-          'https://avatars0.githubusercontent.com/u/53824562?u=a69f9cb990a6f33a40f781c1f726954eae86647a&v=4',
+        avatarUrl: 'https://avatars0.githubusercontent.com/u/53824562?u=a69f9cb990a6f33a40f781c1f726954eae86647a&v=4',
         firstContribution: 2019,
-        thisYearContribution: 51,
+        thisYearContribution: 51
       },
       'sketch-tester': {
         repositoryCount: 0,
@@ -115,7 +103,7 @@ module.exports = {
         pullRequestsCount: 8,
         avatarUrl: 'https://avatars2.githubusercontent.com/u/44187957?v=4',
         firstContribution: 2018,
-        thisYearContribution: 7,
+        thisYearContribution: 7
       },
       'slash-coders': {
         repositoryCount: 0,
@@ -126,7 +114,7 @@ module.exports = {
         pullRequestsCount: 1,
         avatarUrl: 'https://avatars3.githubusercontent.com/u/66586240?v=4',
         firstContribution: 2020,
-        thisYearContribution: 5,
+        thisYearContribution: 5
       },
       subeshb1: {
         repositoryCount: 77,
@@ -135,10 +123,9 @@ module.exports = {
         followersCount: 43,
         issuesCount: 113,
         pullRequestsCount: 261,
-        avatarUrl:
-          'https://avatars0.githubusercontent.com/u/27361350?u=a0ec07685b9692cd9c50ad4b4bb0fdc473064cb5&v=4',
+        avatarUrl: 'https://avatars0.githubusercontent.com/u/27361350?u=a0ec07685b9692cd9c50ad4b4bb0fdc473064cb5&v=4',
         firstContribution: 2017,
-        thisYearContribution: 2156,
+        thisYearContribution: 2156
       },
       WickedInvi: {
         repositoryCount: 0,
@@ -149,7 +136,7 @@ module.exports = {
         pullRequestsCount: 1,
         avatarUrl: 'https://avatars3.githubusercontent.com/u/49752323?v=4',
         firstContribution: 2019,
-        thisYearContribution: 4,
+        thisYearContribution: 4
       },
       KaushikiSinha: {
         repositoryCount: 11,
@@ -158,10 +145,9 @@ module.exports = {
         followersCount: 3,
         issuesCount: 0,
         pullRequestsCount: 1,
-        avatarUrl:
-          'https://avatars0.githubusercontent.com/u/57248094?s=460&u=9b0c945cdab8b81508d1191b0383d6dafc56f04b&v=4',
+        avatarUrl: 'https://avatars0.githubusercontent.com/u/57248094?s=460&u=9b0c945cdab8b81508d1191b0383d6dafc56f04b&v=4',
         firstContribution: 2019,
-        thisYearContribution: 10,
+        thisYearContribution: 10
       },
       vijayakumarGit: {
         repositoryCount: 35,
@@ -170,11 +156,10 @@ module.exports = {
         followersCount: 1,
         issuesCount: 1,
         pullRequestsCount: 15,
-        avatarUrl:
-          'https://avatars3.githubusercontent.com/u/26136684?s=460&v=4',
+        avatarUrl:'https://avatars3.githubusercontent.com/u/26136684?s=460&v=4',
         firstContribution: 2017,
         thisYearContribution: 14,
-      },
+      }
     })
-  },
+  }
 }
